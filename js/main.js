f25182ce6ccfccecc2200a903fe7a67d44999072
hongsms <541058726@qq.com>
2019-04-19 11:37:15 +0800

    1

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var hsm={};hsm.init=function(){hsm.events()};hsm.events=function(){hsm.common()};
+hsm.common = function () {
+// Share
+// window._bd_share_config={"share":{}};with(document){0[(getElementsByTagName("head")[0]||body).appendChild(createElement("script")).src="http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion="+~(-new Date()/3600000)]};  
+// Gotop
+// $(window).scroll(function(){$(window).scrollTop()>500?$("#gotop").show():$("#gotop").hide()});$("#gotop").click(function(){$("body,html").animate({scrollTop:0},1000);return false});
+
+}
+$(function () {
+  hsm.init();
+});
